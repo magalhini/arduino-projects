1584a16eaa8a601101ff1cf03d0c4c4339a24a8a
Ricardo Magalhães <magalhini@gmail.com>
2016-08-02 20:28:38 +0200

    Update weather station with LEDs

--- weather-station/servo.js
+++ weather-station/servo.js
@@ -4,10 +4,11 @@ const five = require('johnny-five');
 const readline = require('readline');
 const fetch = require('node-fetch');
 const apiKey = 'bdcf92bdc2316915261fc4f7de818938';
-const args = process.argv[2] || 'Berlin';
+const args = process.argv[2] || 'Nuuk Greenland';
 const mapRange = require('./helpers').mapRange;
 
 let servo;
+let leds;
 
 function getForecast(place) {
   const weatherURL = 'http://api.openweathermap.org/data/2.5/forecast?' + 'q=' + encodeURIComponent(place) +'&mode=json&units=metric&appid=' + apiKey;
@@ -26,10 +27,14 @@ function getCurrentWeather(weather) {
 function mapTemperatureToValue(weatherData) {
   const temp = weatherData.main.temp;
 
-  if (temp <= 0) return 5;
-  else if (temp <= 10) return 45;
-  else if (temp <= 20) return 90;
-  return 150;
+  console.log('Temp is: ', temp);
+
+  if (temp <= 0) return 0;
+  else if (temp <= 10) return 1;
+  else if (temp <= 15) return 2;
+  else if (temp <= 20) return 3;
+  else if (temp <= 25) return 4;
+  return 6
 }
 
 function moveServo(servo, angle) {
@@ -37,15 +42,26 @@ function moveServo(servo, angle) {
     //servo.to(angle);
 }
 
+function lightUpLeds(value) {
+  leds.forEach((led, index) => {
+    if (index <= value) {
+      led.on();
+    } else {
+      led.off();
+    }
+  });
+}
+
 let range = mapRange(0, 30, 90, 180);
-console.log(range(0));
 
-getForecast(args)
-  .then(getCurrentWeather)
-  .then(mapTemperatureToValue)
-  .then(temp => moveServo(servo, temp))
-  .catch(err => console.log('Ops', err))
-
-/*five.Board().on('ready', function() {
+five.Board().on('ready', function() {
   servo = new five.Servo(9);
-});*/
+  leds = new five.Leds([11,8,7,6,5,4,3]);
+
+  getForecast(args)
+    .then(getCurrentWeather)
+    .then(mapTemperatureToValue)
+    .then(lightUpLeds)
+    .then(temp => moveServo(servo, temp))
+    .catch(err => console.log('Ops', err))
+});
